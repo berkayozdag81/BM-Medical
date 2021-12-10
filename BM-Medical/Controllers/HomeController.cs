4a4b819a0499d404fdd948d85653391412134149
Berkay Özdağ <berkayozdag2001@gmail.com>
2021-12-10 12:09:09 +0300

    Proje dosyası ekle.

---
+++ BM-Medical/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using BM_Medical.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BM_Medical.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
